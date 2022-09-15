2ad274ad21eb15b787445aaf90f080b8fcfbbef2
artaina117 <artaina117@gmail.com>
2022-09-15 17:39:55 +0600

    4.17. Шаблонизируй то

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,19 +1,24 @@
 import AbstractView from '../framework/view/abstract-view.js';
+import dayjs from 'dayjs';
 
-const createFiltersTemplate = () => (`
+const isFuture = (waypoints) => waypoints.some((element) => dayjs(element.dateFrom).diff(dayjs(), 'second') >= 0);
+
+const isPast = (waypoints) => waypoints.some((element) => dayjs(dayjs()).diff(element.dateTo, 'second') >= 0);
+
+const createFiltersTemplate = (waypoints) => (`
   <form class="trip-filters" action="#" method="get">
     <div class="trip-filters__filter">
-      <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything">
+      <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
       <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
     </div>
 
     <div class="trip-filters__filter">
-      <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
+      <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future" ${isFuture(waypoints) ? '' : 'disabled'}>
       <label class="trip-filters__filter-label" for="filter-future">Future</label>
     </div>
 
     <div class="trip-filters__filter">
-      <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past" checked>
+      <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past" ${isPast(waypoints) ? '' : 'disabled'}>
       <label class="trip-filters__filter-label" for="filter-past">Past</label>
     </div>
 
@@ -22,7 +27,14 @@ const createFiltersTemplate = () => (`
 `);
 
 export default class FiltersView extends AbstractView {
+  #waypoints = null;
+
+  constructor (waypointsArray) {
+    super();
+    this.#waypoints = waypointsArray;
+  }
+
   get template() {
-    return createFiltersTemplate();
+    return createFiltersTemplate(this.#waypoints);
   }
 }
